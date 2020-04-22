1a05604a5f736181bcc3102e7c9d4591694d0384
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-04-22 01:17:18 -0300

    Implements repositories and services

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,14 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +22,104 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { transactions } = this;
+
+    /*
+      método utilizando reduce
+    * */
+    const { income, outcome, total } = transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        const { type, value } = transaction;
+        switch (type) {
+          case 'income':
+            accumulator.income += value;
+            accumulator.total += value;
+            break;
+
+          case 'outcome':
+            accumulator.outcome += value;
+            accumulator.total -= value;
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const balance = {
+      total,
+      income,
+      outcome,
+    };
+
+    return balance;
+
+    /*
+      método utilizando filter e reduce
+    * */
+    // const income = transactions
+    //   .filter(transaction => transaction.type === 'income')
+    //   .reduce((sum, transaction) => sum + transaction.value, 0);
+
+    // const outcome = transactions
+    //   .filter(transaction => transaction.type === 'outcome')
+    //   .reduce((sum, transaction) => sum + transaction.value, 0);
+
+    // const total = income - outcome;
+
+    // const balance = {
+    //   income,
+    //   outcome,
+    //   total,
+    // };
+
+    // // console.timeEnd(time);
+
+    // return balance;
+
+    /*
+      método utilizando map
+    * */
+    // let totalIncome = 0;
+    // let totalOutcome = 0;
+
+    // transactions.map(transaction => {
+    //   if (transaction.type === 'income') {
+    //     totalIncome += transaction.value;
+    //   } else if (transaction.type === 'outcome') {
+    //     totalOutcome += transaction.value;
+    //   }
+    // });
+
+    // const total = totalIncome - totalOutcome;
+
+    // const balance = {
+    //   income: totalIncome,
+    //   outcome: totalOutcome,
+    //   total,
+    // };
+
+    // return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Transaction type is not valid.');
+    }
+
+    // Checks if outcome is valid after balance
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+      if (total - value < 0) {
+        throw Error('You have no balance to complete this transaction.');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
